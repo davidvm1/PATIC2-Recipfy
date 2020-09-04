21a9dfcd2ba4f0f2db698bc64cda0fced735499a
davidvm1 <david.vvmm1997@gmail.com>
2020-09-03 22:23:32 -0500

    Singup Layout Icons

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -12,24 +12,37 @@
           label="Nombre"
           required>
         </v-text-field>
-      <v-icon>mdi-account-circle-outline</v-icon>
 
         <v-text-field
+          append-icon="mdi-account-circle-outline"
           outlined
           label="Apellido"
           required>
         </v-text-field>
 
       <v-text-field
+        append-icon="mdi-email-multiple-outline"
         outlined
         type="email"
         label="Correo"
         required>
       </v-text-field>
+
       <v-text-field
+        append-icon="mdi-lock-question"
         outlined
         name="input-10-2"
         label="Contraseña"
+        class="input-group--focused"
+        type="password"
+        @click:append="show3 = !show3">
+    </v-text-field>
+
+    <v-text-field
+        append-icon="mdi-lock-question"
+        outlined
+        name="input-10-2"
+        label="Respite la contraseña"
         class="input-group--focused"
         type="password"
         @click:append="show3 = !show3">
