fe7a65a85de78dc1f7bee86fe8dbcf3df9114c03
davidvmDev <david.velasquezm@upb.edu.co>
2020-09-24 21:45:11 -0500

    serviceConexion

---
+++ src/services/recipesServices.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const appKey = 'ad989c58cfe941d8a4db3f8d72e28d39'
+const recipes = {}
+recipes.getRecipesByIngredients = async (ingredients) => {
+  let ingredientsQuery = ''
+ console.log(ingredients)
+  try {
+   await ingredients.forEach(item => ingredientsQuery = ingredientsQuery + item + ', ' )
+    ingredientsQuery = ingredientsQuery.replace(/, $/,"")
+    const request = {
+      method: 'GET',
+      params: {
+        apiKey: appKey,
+        ingredients: ingredientsQuery
+      },
+      url: `https://api.spoonacular.com/recipes/findByIngredients`
+    }
+
+    const response = await axios(request)
+    return response.data
+  } catch (e) {
+    const message = e.response
+    if (!message) throw new Error(e.message)
+    else throw new Error(message.data.error)
+  }
+}
+
+export default recipes
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,41 +4,71 @@
       <v-img src="../assets/logo.png" height="20vh" width="50%" contain></v-img>
     </v-row>
     <v-row align="center">
-      <v-combobox v-model="chips" chips clearable label="Ingresa tus ingredientes" multiple solo>
-        <template v-slot:selection="{ attrs, item, select, selected }">
-          <v-chip
-            v-bind="attrs"
-            :input-value="selected"
-            close
-            @click="select"
-            @click:close="remove(item)"
-          >
-            <strong>{{ item }}</strong>&nbsp;
-          </v-chip>
+      <v-combobox
+        v-model="ingredients"
+        :items="ingredients"
+        label="Ingresa tus ingredientes"
+        clearable
+        multiple
+        chips
+        deletable-chips
+        persistent-hint>
+        <template v-slot:no-data>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                Preciona <kbd>enter</kbd> para agregar ingredientes
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
         </template>
       </v-combobox>
+
     </v-row>
+
     <v-row justify="center">
-      <v-btn class="ml-2" large>Buscar</v-btn>
+      <v-btn class="ml-2"
+             large
+             @click="getRecipes">
+        Buscar
+      </v-btn>
     </v-row>
+
+    <v-alert class="mt-5"
+             type="error"
+             v-if="error">
+      {{error.message}}
+    </v-alert>
   </v-container>
 </template>
 
 <script>
+  import recipesService from '@/services/recipesServices'
 export default {
   data() {
     return {
       chips: [],
-    };
+      error: '',
+      ingredients: [],
+      recipes: []
+    }
   },
-
   methods: {
     remove(item) {
-      this.chips.splice(this.chips.indexOf(item), 1);
-      this.chips = [...this.chips];
+      this.chips.splice(this.chips.indexOf(item), 1)
+      this.chips = [...this.chips]
     },
+    async getRecipes () {
+      let result
+      try {
+        result = await recipesService.getRecipesByIngredients(this.ingredients)
+        console.log(result)
+      } catch (e) {
+          this.error = e
+      }
+    }
   },
-};
+}
 </script>
 
 <style>
